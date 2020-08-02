d660ca48023a6345df97bbe54ec3fb2601ca6792
Ram <RSA14@ic.ac.uk>
2020-08-03 05:23:20 +0800

    For GPU acceleration using putty console

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import data_processing
 import keras
 import tensorflow as tf
 
-print(tf.config.list_physical_devices())
+# print(tf.config.list_physical_devices())
 # Run process_data first to get cached image data + z-positions
 #STORM data
 # X_train, y_train = data_processing.process_STORM_data('ThunderSTORM/Simulated/LowDensity',
@@ -16,13 +16,13 @@ print(tf.config.list_physical_devices())
 X_train, y_train = data_processing.process_MATLAB_data('../data/Simulated/PSF_toolbox/PSF_2_all.mat',
                                                        '../data/Simulated/PSF_toolbox/Zpos_2_all.mat',
                                                        normalise_images=False)
-print(X_train.shape)
+# print(X_train.shape)
 
 
 # Rescale z-pos
 # y_train = y_train*10
 # y_train_norm, y_mean, y_sd = data_processing.scale_zpos(y_train)
-print(X_train.shape)
+# print(X_train.shape)
 
 #Create resnet model
 
